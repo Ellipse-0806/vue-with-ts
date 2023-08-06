654ad48c2b2c7604b49ad177662dbf3f62710a83
Ellipse-0806 <koin.pokken@gmail.com>
2023-08-07 01:15:49 +0900

    Add test code about vue. - vueファイルが正しく読み込まれるかのテストコード

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,2 +1,8 @@
+import { createApp } from 'vue';
+import welcome from '~/example/welcome.vue';
+
 const id: number = 1;
 console.log(id);
+
+const app = createApp(welcome);
+app.mount('#app');
---
+++ resources/ts/example/welcome.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+console.log("hello Vue!!");
+</script>
+<template>
+    <div class="hello">Hello Vue!!</div>
+</template>
+<style>
+.hello {
+    color: red;
+}
+</style>
