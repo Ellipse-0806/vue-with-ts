9f97970df3e1e451d8ef21a24d7ed43752156e4d
Ellipse-0806 <koin.pokken@gmail.com>
2023-08-07 21:29:34 +0900

    Add test option config. - vueのインポートエラーの回避 - メディアファイルをjsファイルだと認識しないよう、マッパークラスを定義

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,9 +4,14 @@ module.exports = {
     testEnvironment: 'jest-environment-jsdom',
     transform: {
         '.*\\.(vue)$': '@vue/vue3-jest',
-        '.*\\.(ts|tsx)$': 'ts-jest'
+        '.*\\.(ts|tsx)$': 'ts-jest',
     },
     moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/resources/ts/$1'
+        '^@/(.*)$': '<rootDir>/resources/ts/$1',
+        "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+        "<rootDir>/__mocks__/fileMock.js",
+    },
+    testEnvironmentOptions: {
+        customExportConditions: ["node", "node-addons"],
     },
 }
